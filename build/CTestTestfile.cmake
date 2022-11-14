6e4ad055a4fabab146025b614e09488790473964
feixiao <feixiao2020@sina.com>
2022-11-14 19:49:13 +0800

    删除没有用的文件

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/frank/workspace/github/VisualStudioCode
-# Build directory: /Users/frank/workspace/github/VisualStudioCode/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("cmakeQuickStart")

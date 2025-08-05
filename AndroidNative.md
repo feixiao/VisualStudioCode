4e2ebe28554c04b63e88411c53e92ddb0c3bb1b5
feixiao <feixiao2020@sina.com>
2025-08-05 14:58:37 +0800

    准备测试程序

--- AndroidNative.md
+++ AndroidNative.md
@@ -1,7 +1,7 @@
-
+## Android Debugging with Visual Studio Code
 
 
 
 
 #### 参考资料
-+ [](https://www.gamedev.net/tutorials/programming/general-and-gameplay-programming/android-debugging-with-visual-studio-code-r4820/)
++ [《Android Debugging with Visual Studio Code》](https://www.gamedev.net/tutorials/programming/general-and-gameplay-programming/android-debugging-with-visual-studio-code-r4820/)
---
+++ android/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+# For Ubuntu
+
+#export ANDROID_NDK=/opt/ndk/android-ndk-r26d
+
+# For OSX
+export ANDROID_NDK=/Applications/AndroidNDK12479018.app/Contents/NDK
+export PATH=${PATH}:${ANDROID_NDK}/toolchains/llvm/prebuilt/darwin-x86_64/bin
+
+WORKSPACE=$(cd "$dirname "$0")";pwd)
+#APP_ABIs="arm64-v8a armeabi-v7a"
+APP_ABIs="arm64-v8a"
+
+rm -rf $WORKSPACE/build
+
+for APP_ABI in ${APP_ABIs}
+do
+
+cd $WORKSPACE
+mkdir build_${APP_ABI} && cd build_${APP_ABI}
+
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
+ -DANDROID_ABI=${APP_ABI} \
+ -DANDROID_PLATFORM=android-29 \
+ -DCMAKE_SYSTEM_NAME=Android \
+ -DCMAKE_BUILD_TYPE=Debug \
+ -DTARGET_OS_NAME=Android \
+ ../
+make -j4
+
+#rm -rf $WORKSPACE/android_build_${APP_ABI}
+done
+
